9b5240d642124490894319210accc467abefa4d1
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-03-17 09:42:42 +0100

    On ajoute la possibilité de changer de mode d'affichage de l'heure et on modifie le style de la fenêtre pour bien afficher le contenu

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ class Horloge:
         else:
             return None
 
+    def afficher_heure_12h(self):
+        h, m, s = self.heure
+        am_pm = "AM" if h < 12 else "PM"
+        h = h % 12 if h % 12 != 0 else 12
+        return f"{h:02d}:{m:02d}:{s:02d} {am_pm}"
+
 # Fonction pour obtenir l'heure locale sous forme de tuple
 def heure_locale_tuple():
     h, m, s = time.localtime()[3:6]
@@ -36,14 +42,18 @@ class Application(Tk):
     # Initialise l'objet Application, crée les éléments de l'interface graphique et positionne la fenêtre au centre de l'écran
     def __init__(self):
         super().__init__()
+        self.paused = False
+        self.mode_12h = True  # True pour le mode 12 heures, False pour le mode 24 heures
+
+        # Met à jour l'affichage de la fenêtre pour s'assurer que tous les éléments sont créés et ont la bonne taille
+        self.update()
 
         # Centre la fenêtre sur l'écran
-        self.update_idletasks()
         width = self.winfo_screenwidth()
         height = self.winfo_screenheight()
         x = (width - self.winfo_reqwidth()) // 2
         y = (height - self.winfo_reqheight()) // 2
-        self.geometry(f"300x200+{x}+{y}")
+        self.geometry(f"+{x}+{y}")
 
         self.title("Horloge et alarme")
 
@@ -67,14 +77,47 @@ class Application(Tk):
         self.status_label = Label(self, text="", font=big_font, fg=indigo_color)
         self.status_label.pack()
 
+        self.pause_button = Button(self, text="Pause", command=self.pause_clock, font=big_font, fg=indigo_color)
+        self.pause_button.pack(pady=10)
+
+        self.mode_button = Button(self, text="Changer de mode", command=self.toggle_time_mode, font=big_font,
+                                  fg=indigo_color)
+        self.mode_button.pack(pady=10)
+
+        self.heure_label.pack(pady=10, padx=20)  # Ajoute l'option padx
+
+        self.alarme_label.pack(padx=20)  # Ajoute l'option padx
+
+        self.alarme_entry.pack(padx=20)  # Ajoute l'option padx
+
+        self.alarme_button.pack(pady=10, padx=20)  # Ajoute l'option padx
+
+        self.status_label.pack(padx=20)  # Ajoute l'option padx
+
+        self.pause_button.pack(pady=10, padx=20)  # Ajoute l'option padx
+
+        self.mode_button.pack(pady=10, padx=20)  # Ajoute l'option padx
+
         self.update_clock()
+
+    def pause_clock(self):
+        self.paused = not self.paused
+
+    def toggle_time_mode(self):
+        self.mode_12h = not self.mode_12h
 
     # Met à jour l'affichage de l'heure chaque seconde
     def update_clock(self):
-        heure_locale = heure_locale_tuple()
-        horloge = Horloge(heure_locale)
-        heure_text = horloge.afficher_heure()
-        self.heure_label.config(text=heure_text)
+        if not self.paused:
+            heure_locale = heure_locale_tuple()
+            horloge = Horloge(heure_locale)
+
+            if self.mode_12h:
+                heure_text = horloge.afficher_heure_12h()
+            else:
+                heure_text = horloge.afficher_heure()
+
+            self.heure_label.config(text=heure_text)
         self.after(1000, self.update_clock)
 
     # Récupère l'heure de l'alarme entrée par l'utilisateur et planifie la vérification de l'alarme
